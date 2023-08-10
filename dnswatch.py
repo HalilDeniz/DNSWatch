8eeb467539ae5fe97a4f0bcb012ffd248579a193
halil ibrahim deniz <62566319+HalilDeniz@users.noreply.github.com>
2023-08-10 10:49:39 +0300

    Update dnswatch.py

--- dnswatch.py
+++ dnswatch.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+
+
 import requests
 import argparse
 from scapy.all import *
 from colorama import Fore, Style
+from datetime import datetime, timedelta
 from scapy.layers.dns import DNSQR, DNSRR
 from scapy.layers.inet import IP
+from scapy.sendrecv import sniff
 
+from source.dnsdata import DNSDataStorage
 
 dns_requests = {}
 dns_types = {}
@@ -25,31 +31,33 @@ def resolve_dns_doh(dns_request):
             print(f"{Fore.RED}[!] Error resolving DNS over HTTPS: {e}{Style.RESET_ALL}")
 
     return None
-def dns_sniffer(pkt, verbose, output_file, victim_ip=None, analyze_dns_types=False, use_doh=False, filter_domains=[]):
-    if victim_ip and pkt[IP].src != victim_ip and pkt[IP].dst != victim_ip:
+def dns_sniffer(pkt, verbose, output_file, target_ip=None, analyze_dns_types=False, use_doh=False, filter_domains=[], dns_storage=None):
+    if target_ip and pkt[IP].src != target_ip and pkt[IP].dst != target_ip:
         return
+
+    dns_storage = DNSDataStorage()
 
     if pkt.haslayer(DNSQR):
         dns_request = pkt[DNSQR].qname.decode()
         dns_type = pkt[DNSQR].qtype
         dns_src_ip = pkt[IP].src
         dns_dest_ip = pkt[IP].dst
-        timestamp = pkt.time
+        timestamp = datetime.fromtimestamp(pkt.time).strftime('%Y-%m-%d %H:%M:%S')
 
         if filter_domains and not any(domain in dns_request for domain in filter_domains):
             return  # Skip DNS requests that don't match filter domains
 
-        print(f"{Fore.GREEN}[+]{Style.RESET_ALL} DNS Request  from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp:.6f}{Style.RESET_ALL}: {dns_request} (Type: {dns_type})")
+        print(f"{Fore.GREEN}[+]{Style.RESET_ALL} DNS Request  from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp}{Style.RESET_ALL}: {dns_request} (Type: {dns_type})")
 
 
         if use_doh:
             resolved_ips = resolve_dns_doh(dns_request)
             if resolved_ips:
-                print(f"{Fore.GREEN}[+]{Style.RESET_ALL} DNS Request  (DoH) from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp:.6f}{Style.RESET_ALL}: {dns_request} (Type: {dns_type}) - Resolved IPs: {', '.join(resolved_ips)}")
+                print(f"{Fore.GREEN}[+]{Style.RESET_ALL} DNS Request  (DoH) from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp}{Style.RESET_ALL}: {dns_request} (Type: {dns_type}) - Resolved IPs: {', '.join(resolved_ips)}")
             else:
-                print(f"{Fore.GREEN}[+]{Style.RESET_ALL} DNS Request  (DoH) from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp:.6f}{Style.RESET_ALL}: {dns_request} (Type: {dns_type}) - Resolved IPs: (Cannot resolve with DoH)")
+                print(f"{Fore.GREEN}[+]{Style.RESET_ALL} DNS Request  (DoH) from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp}{Style.RESET_ALL}: {dns_request} (Type: {dns_type}) - Resolved IPs: (Cannot resolve with DoH)")
         else:
-            print(f"{Fore.GREEN}[+]{Style.RESET_ALL} DNS Request  from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp:.6f}{Style.RESET_ALL}: {dns_request} (Type: {dns_type})")
+            print(f"{Fore.GREEN}[+]{Style.RESET_ALL} DNS Request  from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp}{Style.RESET_ALL}: {dns_request} (Type: {dns_type})")
 
         if dns_request in dns_requests:
             dns_requests[dns_request][0] += 1
@@ -67,19 +75,21 @@ def dns_sniffer(pkt, verbose, output_file, victim_ip=None, analyze_dns_types=Fal
 
         if output_file:
             with open(output_file, "a") as file:
-                file.write(f"DNS Request  from {dns_src_ip} to {dns_dest_ip} at {timestamp:.6f}: {dns_request} (Type: {dns_type})\n")
+                file.write(f"DNS Request  from {dns_src_ip} to {dns_dest_ip} at {timestamp}: {dns_request} (Type: {dns_type})\n")
+
+        dns_storage.insert_dns_request(timestamp, dns_src_ip, dns_dest_ip, dns_request, dns_type)
 
     if pkt.haslayer(DNSRR):
         dns_response = pkt[DNSRR].rrname.decode()
         dns_type = pkt[DNSRR].type
         dns_src_ip = pkt[IP].src
         dns_dest_ip = pkt[IP].dst
-        timestamp = pkt.time
+        timestamp = datetime.fromtimestamp(pkt.time).strftime('%Y-%m-%d %H:%M:%S')
 
         if filter_domains and not any(domain in dns_response for domain in filter_domains):
             return  # Skip DNS responses that don't match filter domains
 
-        print(f"{Fore.BLUE}[+]{Style.RESET_ALL} DNS Response from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp:.6f}{Style.RESET_ALL}: {dns_response} (Type: {dns_type})")
+        print(f"{Fore.BLUE}[+]{Style.RESET_ALL} DNS Response from {Fore.CYAN}{dns_src_ip}{Style.RESET_ALL} to {Fore.CYAN}{dns_dest_ip}{Style.RESET_ALL} at {Fore.YELLOW}{timestamp}{Style.RESET_ALL}: {dns_response} (Type: {dns_type})")
 
         if dns_response in dns_requests:
             dns_requests[dns_response][1].append(dns_src_ip)
@@ -95,8 +105,19 @@ def dns_sniffer(pkt, verbose, output_file, victim_ip=None, analyze_dns_types=Fal
 
         if output_file:
             with open(output_file, "a") as file:
-                file.write(f"DNS Response from {dns_src_ip} to {dns_dest_ip} at {timestamp:.6f}: {dns_response} (Type: {dns_type})\n")
+                file.write(f"DNS Response from {dns_src_ip} to {dns_dest_ip} at {timestamp}: {dns_response} (Type: {dns_type})\n")
 
+
+            dns_storage.insert_dns_request(timestamp,dns_src_ip,dns_dest_ip, dns_response, dns_type)
+        dns_storage.close()
+
+
+
+"""
+        dns_storage.insert_dns_request(timestamp, dns_src_ip, dns_dest_ip, dns_response, dns_type)
+
+    dns_storage.close()
+"""
 def dns_data_analysis():
     if dns_requests:
         print("\nDNS Data Analysis:")
@@ -123,19 +144,23 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--interface", help="Specify the network interface, for example 'eth0'", required=True)
     parser.add_argument("-v", "--verbose", help="Use this flag to get more verbose output", action="store_true")
     parser.add_argument("-o", "--output", help="Specify the filename to save the results to a file")
-    parser.add_argument("-t", "--target", help="Specify specific target IP address to monitor")
-    parser.add_argument("--analyze-dns-types", help="Use this flag to analyze DNS types", action="store_true")
+    parser.add_argument("-t", "--target-ip", help="Specify specific target IP address to monitor")
+    parser.add_argument("-adt", "--analyze-dns-types", help="Use this flag to analyze DNS types", action="store_true")
     parser.add_argument("--doh", help="DNS over HTTPS (DoH) use this flag to use", action="store_true")
-    parser.add_argument("-fd", "--filter-domains", nargs="+", help="Filter DNS requests by specified domains", default=[])
+    parser.add_argument("-fd", "--target-domains", nargs="+", help="Filter DNS requests by specified domains", default=[])
+    parser.add_argument("-d", "--database", help="Enable database storage", action="store_true")
+
     args = parser.parse_args()
 
     iface = args.interface
     filter_rule = "udp port 53"
 
     try:
+        dns_storage = DNSDataStorage() if args.database else None  # Database Storage'ı isteğe bağlı olarak etkinleştirin
+
         sniff(iface=iface, filter=filter_rule,
-              prn=lambda pkt: dns_sniffer(pkt, args.verbose, args.output, args.target, args.analyze_dns_types,
-                                          args.doh, args.filter_domains))
+              prn=lambda pkt: dns_sniffer(pkt, args.verbose, args.output, args.target_ip, args.analyze_dns_types,
+                                          args.doh, args.target_domains, dns_storage))  # args.filter_domains düzeltiliyor
 
     except PermissionError:
         print(
